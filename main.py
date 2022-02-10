1f9e3bfc045ee8ac55bcfc6fde55ec0114fe2298
rngustj9139 <mnb9139@daum.net>
2022-02-10 22:51:56 +0900

    env token application

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ site = "https://recruit.navercorp.com"
 res = urlopen(site + "/naver/job/list/contents")   # naver career 사이트를 호출하여 응답을 받아온다.
 soup = BeautifulSoup(res, 'html.parser')   # bs4를 이용하여 응답 결과를 html tag 타입으로 파싱한다.
 article_list = soup.select('#content > div.job_fam > #jobListDiv > ul > li')   # 게시글 리스트를 추출(list 형식)
+issueBody = ''
 
 for row in article_list:
     title = row.select("a > span.list_con > strong")[0]
@@ -32,7 +33,6 @@ for row in article_list:
     for row_tag in tag_list:
         allTags += row_tag[0].get_text()
 
-    issueBody = ''
     if('경력' not in allTags and isDateInRange(published_at[0:10]+ ' 00:00:00')): # 등록 시간은 나와있지 않아서 0시라고 가정
         issueBody += item
     else:
@@ -44,7 +44,7 @@ issueTitle = "신입 모집 글 모음(%s)" %(today.strftime("%Y년 %m월 %d일 
 print(issueTitle)
 print(issueBody)
 
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+GITHUB_TOKEN = os.environ['TOKEN_GITHUB']
 REPO_NAME = "recruitment-news"
 repo = Github(GITHUB_TOKEN).get_user().get_repo(REPO_NAME)
 if issueBody != '' and REPO_NAME == repo.name:
